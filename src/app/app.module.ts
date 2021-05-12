edf16c0d9115f16e2e6debc485f71b8c09817b12
Cosmin <xxmlgcosminxx@gmail.com>
2021-05-12 11:39:57 +0200

    mdb angular working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,25 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleInfoComponent } from './article-info/article-info.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomePageComponent
+    HomePageComponent,
+    FooterComponent,
+    ArticlesComponent,
+    ArticleInfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article-info/article-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-info',
+  templateUrl: './article-info.component.html',
+  styleUrls: ['./article-info.component.scss']
+})
+export class ArticleInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
